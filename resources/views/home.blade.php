918148887f026d72df29b0c9d7a9879751ee5b90
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-11-01 23:13:45 +0000

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+    <h1 class="text-teal-500">Test</h1>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Check In</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+@livewireScripts
+</body>
+</html>
