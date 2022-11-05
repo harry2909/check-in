2a588550b6a960a8243fb606ce274d220ebf7219
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-11-05 16:34:26 +0000

    some more styling

--- resources/views/livewire/viewsuccesses.blade.php
+++ resources/views/livewire/viewsuccesses.blade.php
@@ -1,9 +1,9 @@
 <div>
-    <div class="flex justify-center flex-wrap h-auto w-screen items-center px-12">
+    <div class="flex justify-center flex-wrap h-auto w-screen items-center px-24">
         <div class="my-28">
         @foreach($successes as $key => $success)
             <div
-                class="w-full bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 text-center my-6 accordion-container">
+                class="max-w-xs sm:max-w-full w-full bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 text-center my-6 accordion-container">
                 <div class="ac">
                     <div class="p-4 bg-slate-700 rounded-t-lg flex justify-start align-middle items-center ac-trigger">
                         <div class="w-2 h-2 bg-purple-600 mx-2"></div>
@@ -12,13 +12,13 @@
                         <span class="m-auto text-sm text-white font-bold">{{ $key }}</span>
                         <span class="ml-auto text-sm text-white font-bold">Your Successes</span>
                     </div>
-                    <div class="flex flex-wrap flex-row max-h-screen ac-panel overflow-y-auto">
+                    <div class="flex flex-wrap flex-row max-h-screen ac-panel overflow-y-auto max-w-xs sm:max-w-full">
                         <div class="p-12 bg-white ac-text">
-                            <div class="flex justify-start px-4"><h1 class="text-2xl font-bold">{{ $key }}</h1></div>
+                            <div class="flex justify-center lg:justify-start"><h1 class="text-2xl font-bold">{{ $key }}</h1></div>
                             <div class="flex flex-wrap justify-center flex-row">
                                 @foreach($success as $successdata)
                                     <div
-                                        class="w-96 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 m-4 mt-12 mb-12 text-center">
+                                        class="w-full sm:w-80 md:w-52 lg:42 bg-white rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700 m-4 mt-8 mb-8 text-center">
                                         <div
                                             class="p-4 bg-slate-700 rounded-t-lg flex justify-start align-middle items-center">
                                             <div class="w-2 h-2 bg-purple-600 mx-2"></div>
