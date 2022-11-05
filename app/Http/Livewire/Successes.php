51b9e601ae8842c6ee2747212c30767178d09d56
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-11-05 12:28:50 +0000

    styling done, page to submit successes done, dashboard in progress

--- app/Http/Livewire/Successes.php
+++ app/Http/Livewire/Successes.php
@@ -2,21 +2,36 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
 use App\Models\Success;
 
 class Successes extends Component
 {
     public int $currentStep = 1;
-    public String $description;
-    public String $successMessage;
-
+    public String $description = '';
+    public String $currentTime;
     public function render()
     {
+        $this->currentTime = Carbon::now()->toDayDateTimeString();
         return view('livewire.successes');
     }
 
     public function firstStepSubmit() {
         $this->currentStep = 2;
+    }
+
+    public function secondStepSubmit() {
+        $validatedData = $this->validate([
+            'description' => 'required'
+        ]);
+
+        if ($validatedData) {
+            Success::create([
+                'description' => $this->description
+            ]);
+            $this->currentStep = 3;
+        }
+
     }
 }
--- resources/views/livewire/successes.blade.php
+++ resources/views/livewire/successes.blade.php
@@ -6,7 +6,7 @@
             <div class="w-2 h-2 bg-green-300 mx-2"></div>
         </div>
         <div class="p-6">
-            <div class="{{ $currentStep != 1 ? 'hidden' : 'block' }}">
+            <div class="{{ $currentStep != 1 ? 'hidden' : 'flex flex-col' }}">
                 <p class="px-4 mb-2 font-normal text-xl text-gray-700 dark:text-gray-400 text-center">Hey! Let's take
                     the
                     time to note down what went well today &#128522;</p>
@@ -16,13 +16,28 @@
                     Let's go
                 </button>
             </div>
-            <div class="{{ $currentStep != 2 ? 'hidden' : 'block' }} flex flex-col">
-                <label for="description" class="px-4 mb-2 font-normal text-xl text-gray-700 dark:text-gray-400 text-center">Give a description of your success:</label>
-                <textarea id="description" class="mx-4 px-4 py-3 rounded-lg focus:transition focus:ease-in focus:outline-none focus:ring focus:ring-purple-400"></textarea>
+            <div class="{{ $currentStep != 2 ? 'hidden' : 'flex flex-col' }}">
+                <p class="mb-4 px-4 font-normal text-md font-bold text-gray-700 dark:text-gray-400 text-left">
+                    {{ $currentTime }}</p>
+                <label for="description"
+                       class="px-4 mb-3 font-normal text-xl text-gray-700 dark:text-gray-400 text-center">Give a
+                    description of your success &#127881;</label>
+                <textarea wire:model="description" id="description"
+                          class="mx-4 mb-3 px-4 py-3 rounded-lg focus:transition focus:ease-in focus:outline-none focus:ring focus:ring-purple-400 shadow-lg border"></textarea>
+                @error('description') <span class="font-bold text-blue-500">{{ $message }}</span> @enderror
                 <button
                     class="mx-auto w-32 px-6 my-2 py-2 text-white text-lg mt-3 rounded-md bg-gradient-to-r from-purple-600 via-blue-500 to-green-300 background-animate hover:animate-bounce"
-                    wire:click="firstStepSubmit">
+                    wire:click="secondStepSubmit">
                     Submit
+                </button>
+            </div>
+            <div class="{{ $currentStep != 3 ? 'hidden' : 'flex flex-col' }}">
+                <p class="px-4 mb-2 font-normal text-xl text-gray-700 dark:text-gray-400 text-center">Great job! Your
+                    success has been saved &#128522;</p>
+                <button
+                    class="mx-auto w-32 px-6 my-2 py-2 text-white text-lg mt-3 rounded-md bg-gradient-to-r from-purple-600 via-blue-500 to-green-300 background-animate hover:animate-bounce"
+                    wire:click="submitForm">
+                    Dashboard
                 </button>
             </div>
         </div>
