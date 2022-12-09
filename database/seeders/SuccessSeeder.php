9c87d16a06a93d619aa57e9b8d4a1576430a5019
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-12-09 17:58:17 +0000

    styling

--- database/seeders/SuccessSeeder.php
+++ database/seeders/SuccessSeeder.php
@@ -14,6 +14,6 @@ class SuccessSeeder extends Seeder
      */
     public function run()
     {
-       Success::factory()->count(1000)->create();
+       Success::factory()->count(100)->create();
     }
 }
--- resources/views/livewire/viewsuccesses.blade.php
+++ resources/views/livewire/viewsuccesses.blade.php
@@ -23,7 +23,7 @@
                             <div class="w-2 h-2 bg-blue-500 mx-2"></div>
                             <div class="w-2 h-2 bg-green-300 mx-2 mr-auto"></div>
                             <span class="m-auto text-sm text-white font-bold">{{ $key }}</span>
-                            <span class="ml-auto text-sm text-white font-bold">Your Successes</span>
+                            <span class="ml-auto text-sm text-white font-bold">Count: {{ count($successes[$key]) }}</span>
                         </div>
                         <div
                             class="flex flex-wrap flex-row max-h-screen ac-panel overflow-y-auto max-w-xs sm:max-w-full rounded-lg">
@@ -33,7 +33,7 @@
                                 <div class="flex flex-wrap justify-center flex-row">
                                     @foreach($success as $successdata)
                                         <div
-                                            class="w-full sm:w-80 md:w-52 lg:42 bg-white rounded-lg shadow-2xl  m-4 mt-8 mb-8 text-center">
+                                            class="w-full bg-white rounded-lg shadow-2xl  m-4 mt-8 mb-8 text-center">
                                             <div
                                                 class="p-4 bg-slate-700 rounded-t-lg flex justify-start align-middle items-center">
                                                 <div class="w-2 h-2 bg-purple-600 mx-2"></div>
